330007c9b96d5e9fbcdf1b6a12ce5ab3afe54bde
procadiana <diana.claudia.ilinca@gmail.com>
2019-05-14 21:41:58 +0000

    First exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,4 +1,23 @@
 require_relative '../setup'
+#### Exercise 1: Create 3 stores
+
+# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+#   * Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+#   * Richmond (annual_revenue of 1260000 carries women's apparel only)
+#   * Gastown (annual_revenue of 190000 carries men's apparel only)
+# 2. Output (`puts`) the number of the stores using ActiveRecord's `count` method, to ensure that there are three stores in the database.
+
+class Store < ActiveRecord::Base
+
+end
+
+burnaby = Store.create(annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+richmond = Store.create(annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+gastown = Store.create(annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "We have #{Store.count} stores"
 
 puts "Exercise 1"
 puts "----------"
